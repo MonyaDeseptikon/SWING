8ee78f5c34ba2fb148e86111ec6035541c3b7f04
Monya <isadom@mail.ru>
2025-04-27 13:26:50 +0400

    добавлен ввод размерности

--- src/SettingWindow.java
+++ src/SettingWindow.java
@@ -1,31 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static java.util.Objects.isNull;
+
 public class SettingWindow extends JFrame {
-//    private static final int WINDOW_HEIGHT = 230; //555;
-//    private static final int WINDOW_WIDTH = 350; //507;
-
     JButton btnStart = new JButton("Старт");
-
+    JTextField fieldLenInput = new JTextField("3", 1);
+    int fieldLen;
 
     SettingWindow(GameWindow gameWindow) {
 
         setResizable(false);
         setSize(gameWindow.getSize());
 
-        add(btnStart);
 
+        add(btnStart, BorderLayout.SOUTH);
+        add(fieldLenInput, BorderLayout.NORTH);
+        fieldLenInput.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fieldLen = Integer.parseInt(fieldLenInput.getText());
+            }
+        });
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-
-btnStart.addActionListener(new ActionListener() {
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        gameWindow.map.startNewGame(0,6,6,6);
-        setVisible(false);
-    }
-});
-
+                gameWindow.map.startNewGame(0, fieldLen, fieldLen, fieldLen);
+                setVisible(false);
+            }
+        });
 
 
     }
